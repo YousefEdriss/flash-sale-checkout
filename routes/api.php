8fc0ad592338013b9ce085dea682187c20f021a0
YousefEdriss <108696851+YousefEdriss@users.noreply.github.com>
2025-12-01 22:25:07 +0200

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HoldController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\WebhookController;
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/holds', [HoldController::class, 'store']);
+Route::post('/orders', [OrderController::class, 'store']);
+Route::post('/payments/webhook', [WebhookController::class, 'handle']);
