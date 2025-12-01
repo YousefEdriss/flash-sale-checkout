71b1f1545a898b9f806fceb95ee8761add5f5bec
YousefEdriss <108696851+YousefEdriss@users.noreply.github.com>
2025-12-01 22:23:06 +0200

    Create Order.php

---
+++ app/Models/Order.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['hold_id','status','payment_ref'];
+    public function hold(){ return $this->belongsTo(Hold::class); }
+}
